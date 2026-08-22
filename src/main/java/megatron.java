bba6760d3b9e720fe2f8aae784b89aa517402b34
juniuslui <jun.lui.q.f@gmail.com>
2026-08-23 00:55:58 +0800

    Add support for echo

---
+++ src/main/java/megatron.java
@@ -0,0 +1,13 @@
+public class megatron {
+    public static void main(String[] args) {
+        String banner = " __  __ _____ ____    _  _____ ____   ___  _   _\n"
+                + "|  \\/  | ____/ ___|  / \\|_   _|  _ \\ / _ \\| \\ | |\n"
+                + "| |\\/| |  _|| |  _  / _ \\ | | | |_) | | | |  \\| |\n"
+                + "| |  | | |__| |_| |/ ___ \\| | |  _ <| |_| | |\\  |\n"
+                + "|_|  |_|_____\\____/_/   \\_\\|_| |_| \\_\\___/|_| \\_|\n";
+        System.out.println(banner);
+        System.out.println("Hello! I'm megatron.");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
